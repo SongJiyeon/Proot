f2b1fbe189b587b5dc061b278ea61dabeda581e3
Songjiyeon <jiyeon.song@riiid.co>
2023-11-15 11:01:53 +0900

    Initial Commit

---
+++ Proot/ProotApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProotApp.swift
+//  Proot
+//
+//  Created by 송지연 on 11/15/23.
+//
+
+import SwiftUI
+
+@main
+struct ProotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
